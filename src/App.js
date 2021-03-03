14bf30fd4723e7572055063f2d95de5f349fd670
Ghulam Mohi ud Din <ghulam.mohiuddin@Powersoft19.com>
2021-03-03 22:28:47 +0500

    Data fetched from openweather API

--- src/App.js
+++ src/App.js
@@ -2,28 +2,35 @@ import React, {useState, useEffect} from 'react';
 import './App.css';
 import WeatherCard from './components/WeatherCard/WeatherCard';
 
+const url = 'http://api.openweathermap.org/data/2.5/weather?q=abbottabad&units=metric&appid=fa66506ffa308c3cc8af36bdb1e93347';
 function App() {
-	const [var_data, set_data] = useState(null)
-	useEffect(() => {
-		fetchData();
-		
-	}, [])
+	const [forecast, setForecast] = useState([]);
+	const [error, setError] = useState();
 
-	async function fetchData() {
-		const url = 'http://api.openweathermap.org/data/2.5/weather?q=lahore&units=metric&appid=fa66506ffa308c3cc8af36bdb1e93347';
-		try{
+	const fetchForecast = async () => {
+		try {
 			const response = await fetch(url);
-			const data = await response.json();
-			set_data(data.main);
-			console.log(var_data);
-		} catch(err) {
-			console.log(err)
+			const responseJson = await response.json();
+			setForecast(responseJson);
+		} catch (error) {
+			setError(error)
 		}
 	}
+	
+	useEffect( () => {
+		fetchForecast()
+	}, [])
 
+	console.log(forecast.main);
 	return (
 		<div className="App">
-			<WeatherCard temp={var_data.temp} />
+			<WeatherCard
+				city={forecast.name}
+				temp={forecast.main.temp}
+				humidity={forecast.main.humidity}
+				windSpeed={forecast.wind.speed}
+				degree={forecast.wind.speed}
+				weather={forecast.weather[0].main}  />
 		</div>
 	);
 }
--- src/components/WeatherCard/WeatherCard.js
+++ src/components/WeatherCard/WeatherCard.js
@@ -2,13 +2,25 @@ import React from 'react';
 
 import './WeatherCard.css';
 
-export default function WeatherCard({temp}) {
+export default function WeatherCard({city, temp, humidity, windSpeed, degree, weather}) {
     const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Satureday'];
     const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
     const newDate = new Date();
     const today = newDate.getDay();
     const todayDate = newDate.getDate();
     const month = newDate.getMonth();
+
+    const direction = (degree) => {
+        if (degree>337.5) return 'North';
+        if (degree>292.5) return 'North West';
+        if (degree>247.5) return 'West';
+        if (degree>202.5) return 'South West';
+        if (degree>157.5) return 'South';
+        if (degree>122.5) return 'South East';
+        if (degree>67.5) return 'East';
+        if (degree>22.5){return 'North East';}
+        return 'North';
+    }
 
     return (
         <div className="card">
@@ -17,17 +29,23 @@ export default function WeatherCard({temp}) {
                 <div className="date">{todayDate} {months[month]}</div>
             </div>
             <div className="card_content">
-                <div className="location">Lahore</div>
+                <div className="location">{city}</div>
                 <div className="degree">
-                    <div className="num">{temp} c</div>
+                    <div className="num">{Math.round(temp)} c</div>
                     <div className="weather_icon">
-                        <img src="/icons/icon-1.svg" alt="" />
+                        { weather === 'Thunderstorm' && <img src="/icons/icon-12.svg" alt="" />}
+                        { weather === 'Drizzle' && <img src="/icons/icon-9.svg" alt="" />}
+                        { weather === 'Rain' && <img src="/icons/icon-10.svg" alt="" />}
+                        { weather === 'Snow' && <img src="/icons/icon-14.svg" alt="" />}
+                        { weather === 'Haze' || weather === 'Mist' || weather === 'Smoke' || weather === 'Dust' || weather === 'Fog' && <img src="/icons/icon-8.svg" alt="" />}
+                        { weather === 'Clear' && <img src="/icons/icon-2.svg" alt="" />}
+                        { weather === 'Clouds' && <img src="/icons/icon-6.svg" alt="" />}
                     </div>
                 </div>
                 <div className="weather_info">
-                    <div><img src="/icons/icon-umberella.png" alt="" /><div>20%</div></div>
-                    <div><img src="/icons/icon-wind.png" alt="" /><div>18km/h</div></div>
-                    <div><img src="/icons/icon-compass.png" alt="" /><div>East</div></div>
+                    <div><img src="/icons/icon-umberella.png" alt="" /><div>{humidity}%</div></div>
+                    <div><img src="/icons/icon-wind.png" alt="" /><div>{windSpeed} m/s</div></div>
+                    <div><img src="/icons/icon-compass.png" alt="" /><div>{direction(degree)}</div></div>
                 </div>
             </div>
         </div>
